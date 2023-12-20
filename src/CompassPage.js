ca7c3e7b83657017f23d376f83133a286fb4be0e
yopiangga <yopigambyok@gmail.com>
2023-12-20 20:57:15 +0700

    init

---
+++ src/CompassPage.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export function CompassPage() {
+  const [degrees, setDegrees] = useState(0);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("deviceorientation", (event) => {
+      setDegrees(event.alpha);
+      if (event.alpha >= 20 && event.alpha <= 21) {
+        navigator.vibrate(500);
+      }
+    });
+    navigator.permissions.query({ name: "geolocation" });
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation(position);
+    });
+  }, []);
+  return (
+    <div>
+      <h1 style={{ color: "black" }}>Compass</h1>
+      <p>{degrees ?? "-"}</p>
+      <p>{location?.coords.latitude ?? "-"}</p>
+    </div>
+  );
+}
